1bb5da42e5291455c117c90302f86fd80e42da2c
whatislove118 <oleg.pochikalin@mail.ru>
2021-07-06 19:21:13 +0300

    add_download_button

--- core/ui/user_interface.py
+++ core/ui/user_interface.py
@@ -2,15 +2,9 @@ import PySimpleGUI as sg
 
 font = ('Arial, 15')
 
-# choose_audio_scrolling_text = [
-#     ]
-#         sg.InputText(size=(44, 100))
-#     ]
-# ]
 
 
-
-icon_column = [
+info_column = [
     [
         sg.Text('Title', pad=((10, 63), (15, 0))),
         sg.InputText(size=(44, 100), pad=((1, 30), (10, 0))),
@@ -22,16 +16,16 @@ icon_column = [
     ],
     [
         sg.Text('Choose audio', pad=((10, 0), (4, 4))),
-        sg.Button(button_text='Browse...', pad=((0, 93), (4, 4)), enable_events=True),
+        sg.Button(button_text='Browse...', key='__BROWSE_AUDIO__', pad=((0, 93), (4, 4)), enable_events=True),
         sg.Text('default', key='_AUDIO_NAME_'),
-        sg.Button(button_text='Play', image_filename='core/ui/static/play-button.png', image_size=(20, 20), pad=((130, 0), (4, 4))),
-        sg.Button(button_text='Stop', image_filename='core/ui/static/pause-button.png', image_size=(20, 20), pad=((10, 0), (4, 4)))
+        sg.Button(button_text='Play', key='__PLAY_AUDIO__', image_filename='core/ui/static/play-button.png', image_size=(20, 20), pad=((130, 0), (4, 4))),
+        sg.Button(button_text='Stop', key='__STOP_AUDIO__', image_filename='core/ui/static/pause-button.png', image_size=(20, 20), pad=((10, 0), (4, 4)))
     ]
 ]
 
 image_column = [
     [
-        sg.Image(filename='core/ui/static/plus.png', size=(80, 80)),
+        sg.Image(filename='core/ui/static/plus.png', size=(80, 80), enable_events=True, key='__ICON__'),
     ]
 ]
 
@@ -39,7 +33,7 @@ layout = [
     [
         # sg.Column(layout=icon_column, vertical_alignment='center', justification='center')
         sg.Column(layout=image_column, vertical_alignment='left', justification='center', pad=((0, 0), (10, 10))),
-        sg.Column(layout=icon_column, vertical_alignment='center', justification='center'),
+        sg.Column(layout=info_column, vertical_alignment='center', justification='center'),
     ],
 
     # [
@@ -52,19 +46,24 @@ layout = [
     # [sg.Submit(), sg.Cancel()]
 ]
 
-# TODO fix bug
 def popup_select(elements):
     layout_popup = [
         [
-            sg.Listbox(elements, key='_AUDIO_', size=(45, len(elements)), highlight_background_color='green',
-                       highlight_text_color='white', select_mode='extended', bind_return_key=True),
         ],
         [
-            sg.OK(pad=((4, 215), (10, 10))),
+            sg.OK(pad=((4, 160), (10, 10))),
+            sg.Button(button_text='Download', button_color='blue', key='__DOWNLOAD_AUDIO__', pad=((0, 132), (10, 10))),
             sg.Cancel(button_color='red')
         ]
     ]
-    window_popup = sg.Window('Choose audio name', layout=layout_popup)
+
+    layout_popup[0].append(
+        sg.Listbox(
+                    elements, key='_AUDIO_', size=(45, len(elements)), highlight_background_color='green',
+                    highlight_text_color='white', select_mode='extended', bind_return_key=True
+        )
+    )
+    window_popup = sg.Window('Choose audio name', layout=layout_popup, font=font)
     event_popup, values_popup = window_popup.read()
     window_popup.close()
     del window_popup
--- main.py
+++ main.py
@@ -9,29 +9,31 @@ import PySimpleGUI as sg
 from pygame import mixer
 
 from core.ui.user_interface import layout, font, popup_select
-# TODO add check music
+
 if __name__ == '__main__':
     sg.theme('Dark')
     mixer.init()
     window = sg.Window("LetMeKnow", layout, font=font)
-    while True:  # The Event Loop
+    while True:
         event, values = window.read()
-        print(event, values) #debug
+        print(event, values)  # debug
         if event in (None, 'Exit', 'Cancel'):
             break
-        if event == 'Browse...':
+        if event == '__BROWSE_AUDIO__':
             local_audio_finder = LocalAudioFinder.getInstance()
             files = local_audio_finder.find_all()
             audio_name = popup_select(local_audio_finder.audio_list)
             window['_AUDIO_NAME_'].update(audio_name)
-        if event == 'Play':
+        if event == '__PLAY_AUDIO__':
             # TODO add multithreading
             local_audio_finder = LocalAudioFinder.getInstance()
             audio_name = window['_AUDIO_NAME_'].get() + '.mp3'
             mixer.music.load(audio_name)
             mixer.music.play()
-        if event == 'Stop':
+        if event == '__STOP_AUDIO__':
             mixer.music.stop()
+        if event == '__BROWSE_ICON__':
+            pass
 
 
 
